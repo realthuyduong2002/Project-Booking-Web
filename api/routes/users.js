37c428ad8224e48b60d63676a5252ad7e8c31e3e
khatu2002 <20522096@gm.uit.edu.vn>
2023-11-24 16:32:36 +0700

    Update users.js

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { updateUser, deleteUser, getUser, getUsers } from "../controllers/user.js";
-import { verifyToken, verifyUser, verifyAdmin } from "../utils/verifyToken.js";
+import { verifyUser, verifyAdmin } from "../utils/verifyToken.js";
 
 
 const router = express.Router();
