508cf30c899d1e93594dc951b1df9e5eda7744c5
Dưn <115691542+realthuyduong2002@users.noreply.github.com>
2023-12-14 08:22:54 +0700

    Add files via upload

---
+++ api/routes/users.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { updateUser, deleteUser, getUser, getUsers } from "../controllers/user.js";
+import { verifyAdmin, verifyUser } from "../utils/verifyToken.js";
+
+
+const router = express.Router();
+
+
+//UPDATE
+router.put("/:id", updateUser)
+//DELETE
+router.delete("/:id", deleteUser)
+//GET
+router.get("/:id", getUser)
+//GET ALL
+router.get("/", getUsers)
+export default router
